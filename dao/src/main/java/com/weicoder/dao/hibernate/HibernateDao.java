8041e57a5a03e448d73dbdc48d6d146d0587ce9a
unknown <wudi-hj@service.360ityun.com>
2018-06-15 18:56:15 +0800

    wdcode

--- dao/src/main/java/com/weicoder/dao/hibernate/HibernateDao.java
+++ dao/src/main/java/com/weicoder/dao/hibernate/HibernateDao.java
@@ -61,7 +61,7 @@ public final class HibernateDao implements Dao {
 			// 循环添加
 			for (E e : entitys) {
 				session.save(e);
-				session.flush();
+//				session.flush();
 			}
 			// 返回实体
 			return entitys;
