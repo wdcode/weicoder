5073168f10bcee7919127bf95c7255dc57b63e2d
wudi <3882764@qq.com>
2019-11-27 21:23:12 +0800

    wdcode

--- dao/src/main/java/com/weicoder/dao/hibernate/tx/HibernateTransactional.java
+++ dao/src/main/java/com/weicoder/dao/hibernate/tx/HibernateTransactional.java
@@ -7,18 +7,20 @@ import com.weicoder.dao.Transactional;
 
 /**
  * Hibernate 事务类
+ * 
  * @author WD
  */
 public final class HibernateTransactional implements Transactional {
 	// 保存的Session
-	private Session		session;
+	private Session session;
 	// 保存事务
-	private Transaction	tx;
+	private Transaction tx;
 	// 事务状态是否开始
-	private boolean		begin;
+	private boolean begin;
 
 	/**
 	 * 构造方法
+	 * 
 	 * @param session hibernate session
 	 */
 	public HibernateTransactional(Session session) {
@@ -26,21 +28,24 @@ public final class HibernateTransactional implements Transactional {
 	}
 
 	@Override
-	public void begin() {
+	public Transactional begin() {
 		tx = session.beginTransaction();
 		begin = true;
+		return this;
 	}
 
 	@Override
-	public void commit() {
+	public Transactional commit() {
 		tx.commit();
 		begin = false;
+		return this;
 	}
 
 	@Override
-	public void rollback() {
+	public Transactional rollback() {
 		tx.rollback();
 		begin = false;
+		return this;
 	}
 
 	@Override
--- web/src/main/java/com/weicoder/web/validator/annotation/Token.java
+++ web/src/main/java/com/weicoder/web/validator/annotation/Token.java
@@ -54,6 +54,7 @@ public @interface Token {
 	
 	/**
 	 * 如果不为空 强制赋值不管是否有用户传入
+	 * 
 	 * @return 强制参数
 	 */
 	String uid() default StringConstants.EMPTY;
