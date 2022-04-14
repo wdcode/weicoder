234fde4f66ed458bec8fd55c73f45bd6b4e820aa
wdcode <wdcode@qq.com>
2022-04-14 17:51:56 +0800

    wdcode

--- xml/src/main/java/com/weicoder/xml/XmlEngine.java
+++ xml/src/main/java/com/weicoder/xml/XmlEngine.java
@@ -1,7 +1,8 @@
 package com.weicoder.xml;
 
 import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.io.xml.Dom4JDriver; 
+import com.thoughtworks.xstream.io.xml.Dom4JDriver;
+import com.weicoder.common.U.B;
 
 /**
  * XML处理引擎
@@ -15,8 +16,8 @@ public final class XmlEngine {
 	/**
 	 * 把实体对象转换成xml字符串
 	 * 
-	 * @param  obj 要转换的实体对象
-	 * @return     转换后的字符串
+	 * @param obj 要转换的实体对象
+	 * @return 转换后的字符串
 	 */
 	public static String toXML(Object obj) {
 		return STREAM.toXML(obj);
@@ -25,8 +26,8 @@ public final class XmlEngine {
 	/**
 	 * 把xml字符串转换成实体对象
 	 * 
-	 * @param  xml xml字符串
-	 * @return     实体对象
+	 * @param xml xml字符串
+	 * @return 实体对象
 	 */
 	public static Object toBean(String xml) {
 		return STREAM.fromXML(xml);
@@ -35,14 +36,15 @@ public final class XmlEngine {
 	/**
 	 * 把xml字符串转换成特定实体对象
 	 * 
-	 * @param  xml   xml字符串
-	 * @param  clazz 要转换的类型
-	 * @param  <T>   范型
-	 * @return       特定实体对象
+	 * @param xml   xml字符串
+	 * @param clazz 要转换的类型
+	 * @param <T>   范型
+	 * @return 特定实体对象
 	 */
-	@SuppressWarnings("unchecked")
 	public static <T> T toBean(String xml, Class<T> clazz) {
-		return (T) toBean(xml);
+		STREAM.allowTypes(new Class[] { clazz });
+		return B.copy(toBean(xml), clazz);
+//		return (T)toBean(xml);
 	}
 
 	private XmlEngine() {
